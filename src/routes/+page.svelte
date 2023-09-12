49207af67ea8f2ff46334093a4e31d55d2e41ee7
Zach Jensz <zach@zachjensz.com>
2023-09-12 15:25:28 +1000

    Added basic calculation logic

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,32 @@
+<script>
+	let emptyWeight = 1521.45;
+	let emptyArm = 86.18;
+	let frontWeight = 0;
+	let frontArm = 0;
+	let fuelWeight = 0;
+	let fuelArm = 0;
+	let rearWeight = 0;
+	let rearArm = 0;
+	let baggageWeight = 0;
+	let baggageArm = 0;
+
+	$: totalWeight = twoDecimals(emptyWeight + frontWeight + fuelWeight + rearWeight + baggageWeight);
+	$: totalMoment = twoDecimals(
+		moment(emptyWeight, emptyArm) +
+			moment(frontWeight, frontArm) +
+			moment(fuelWeight, fuelArm) +
+			moment(rearWeight, rearArm) +
+			moment(baggageWeight, baggageArm)
+	);
+
+	function moment(weight, arm) {
+		return twoDecimals(weight * arm);
+	}
+	function twoDecimals(number) {
+		return Math.round((number + Number.EPSILON) * 100) / 100;
+	}
+</script>
+
 <h1>Piper Warrior Weight and Balance Calculator</h1>
 <table>
 	<tr>
@@ -8,39 +37,41 @@
 	</tr>
 	<tr>
 		<th>Empty Weight</th>
-		<td><input type="number" value="199" /></td>
-		<td><input type="number" /></td>
-		<td><output /></td>
+		<td><input id="empty-weight" type="number" bind:value={emptyWeight} /></td>
+		<td><input id="empty-arm" type="number" bind:value={emptyArm} /></td>
+		<td><output id="empty-moment">{moment(emptyWeight, emptyArm)}</output></td>
 	</tr>
 	<tr>
 		<th>Front Seats</th>
-		<td><input type="number" /></td>
-		<td><input type="number" /></td>
-		<td><output /></td>
+		<td><input id="front-weight" type="number" bind:value={frontWeight} /></td>
+		<td><input id="front-arm" type="number" bind:value={frontArm} /></td>
+		<td><output id="front-moment">{moment(frontWeight, frontArm)}</output></td>
 	</tr>
 	<tr>
 		<th>Fuel</th>
-		<td><input type="number" /></td>
-		<td><input type="number" /></td>
-		<td><output /></td>
+		<td><input id="fuel-weight" type="number" bind:value={fuelWeight} /></td>
+		<td><input id="fuel-arm" type="number" bind:value={fuelArm} /></td>
+		<td><output id="fuel-moment">{moment(fuelWeight, fuelArm)}</output></td>
 	</tr>
 	<tr>
 		<th>Rear Seats</th>
-		<td><input type="number" /></td>
-		<td><input type="number" /></td>
-		<td><output /></td>
+		<td><input id="rear-weight" type="number" bind:value={rearWeight} /></td>
+		<td><input id="rear-arm" type="number" bind:value={rearArm} /></td>
+		<td><output id="rear-moment">{moment(rearWeight, rearArm)}</output></td>
 	</tr>
 	<tr>
 		<th>Baggage</th>
-		<td><input type="number" /></td>
-		<td><input type="number" /></td>
-		<td><output /></td>
+		<td><input id="baggage-weight" type="number" bind:value={baggageWeight} /></td>
+		<td><input id="baggage-arm" type="number" bind:value={baggageArm} /></td>
+		<td><output id="baggage-moment">{moment(baggageWeight, baggageArm)}</output></td>
 	</tr>
 	<tr>
 		<th>Total</th>
-		<td><output /></td>
-		<td><output /></td>
-		<td><output /></td>
+		<td><output>{totalWeight}</output></td>
+		<td><output>{twoDecimals(totalMoment / totalWeight)}</output></td>
+		<td>
+			<output>{totalMoment}</output>
+		</td>
 	</tr>
 </table>
 
