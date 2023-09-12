2c0476887262c3e741533e144517862c679da476
Zach Jensz <zach@zachjensz.com>
2023-09-12 14:42:10 +1000

    Added basic table markup and styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,65 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Piper Warrior Weight and Balance Calculator</h1>
+<table>
+	<tr>
+		<th />
+		<th>Weight (lb)</th>
+		<th>Arm (in)</th>
+		<th>Moment (lb.in)</th>
+	</tr>
+	<tr>
+		<th>Empty Weight</th>
+		<td><input type="number" value="199" /></td>
+		<td><input type="number" /></td>
+		<td><output /></td>
+	</tr>
+	<tr>
+		<th>Front Seats</th>
+		<td><input type="number" /></td>
+		<td><input type="number" /></td>
+		<td><output /></td>
+	</tr>
+	<tr>
+		<th>Fuel</th>
+		<td><input type="number" /></td>
+		<td><input type="number" /></td>
+		<td><output /></td>
+	</tr>
+	<tr>
+		<th>Rear Seats</th>
+		<td><input type="number" /></td>
+		<td><input type="number" /></td>
+		<td><output /></td>
+	</tr>
+	<tr>
+		<th>Baggage</th>
+		<td><input type="number" /></td>
+		<td><input type="number" /></td>
+		<td><output /></td>
+	</tr>
+	<tr>
+		<th>Total</th>
+		<td><output /></td>
+		<td><output /></td>
+		<td><output /></td>
+	</tr>
+</table>
+
+<style>
+	input,
+	output {
+		all: unset;
+		display: block;
+		width: 16ch;
+		height: 1.2em;
+		padding: 0.2em;
+		margin-block: 0.2em;
+		border: 1px solid #aaa;
+	}
+	th {
+		font-weight: unset;
+		text-align: unset;
+	}
+	th:first-child {
+		padding-inline-end: 1rem;
+	}
+</style>
