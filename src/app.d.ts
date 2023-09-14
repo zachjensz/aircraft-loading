e8dd02095b1b1ca6eb7c8332b49c2a3eeed22896
Zach Jensz <zach@zachjensz.com>
2023-09-14 14:33:25 +1000

    Added cloudflare sveltekit adapter

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,11 +1,19 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 declare global {
-	namespace App {
-		// interface Error {}
-		// interface Locals {}
-		// interface PageData {}
-		// interface Platform {}
+	declare namespace App {
+		interface Locals {}
+		interface Platform {
+			env: {
+				COUNTER: DurableObjectNamespace;
+			};
+			context: {
+				waitUntil(promise: Promise<any>): void;
+			};
+			caches: CacheStorage & { default: Cache };
+		}
+		interface Session {}
+		interface Stuff {}
 	}
 }
 
