93310ae4c8a1da87ec7a8785b0842c7a71ef5207
Zach Jensz <zach@zachjensz.com>
2023-10-18 16:28:51 +1100

    Simplified fix to masses not updating

--- src/lib/MassItem.svelte
+++ src/lib/MassItem.svelte
@@ -2,6 +2,7 @@
 	import { currentAircraft } from '$lib/loadingData.ts';
 
 	export let massIndex;
+	let mass = $currentAircraft.masses[massIndex];
 
 	function twoFrac(number) {
 		return Math.round((number + Number.EPSILON) * 100) / 100;
@@ -9,27 +10,24 @@
 </script>
 
 <tr>
-	<th>{$currentAircraft.masses[massIndex].name}</th>
+	<th>{mass.name}</th>
 	<td
 		><input
-			id={`${$currentAircraft.masses[massIndex].name}-weight`}
+			id={`${mass.name}-weight`}
 			type="number"
 			bind:value={$currentAircraft.masses[massIndex].weightPounds}
 		/></td
 	>
 	<td
 		><input
-			id={`${$currentAircraft.masses[massIndex].name}-arm`}
+			id={`${mass.name}-arm`}
 			type="number"
 			bind:value={$currentAircraft.masses[massIndex].armInches}
 		/></td
 	>
 	<td
-		><output id={`${$currentAircraft.masses[massIndex].name}-moment`}
-			>{twoFrac(
-				$currentAircraft.masses[massIndex].weightPounds *
-					$currentAircraft.masses[massIndex].armInches
-			)}</output
+		><output id={`${mass.name}-moment`}
+			>{twoFrac(mass.weightPounds * mass.armInches)}</output
 		></td
 	>
 </tr>
