1624bdc8d5b4911ca7a6840eb8ac304a944633dc
Zach Jensz <zach@zachjensz.com>
2023-10-22 12:16:23 +1100

    Individual masses moments now update

--- src/lib/MassItem.svelte
+++ src/lib/MassItem.svelte
@@ -2,7 +2,7 @@
 	import { currentAircraft } from '$lib/loadingData.ts';
 
 	export let massIndex;
-	let mass = $currentAircraft.masses[massIndex];
+	$: mass = $currentAircraft.masses[massIndex];
 
 	function twoFrac(number) {
 		return Math.round((number + Number.EPSILON) * 100) / 100;
@@ -15,14 +15,14 @@
 		><input
 			id={`${mass.name}-weight`}
 			type="number"
-			bind:value={$currentAircraft.masses[massIndex].weightPounds}
+			bind:value={mass.weightPounds}
 		/></td
 	>
 	<td
 		><input
 			id={`${mass.name}-arm`}
 			type="number"
-			bind:value={$currentAircraft.masses[massIndex].armInches}
+			bind:value={mass.armInches}
 		/></td
 	>
 	<td
